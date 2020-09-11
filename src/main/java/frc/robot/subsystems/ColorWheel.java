d1bcadfc1b91c3d3851c3599faa74072c2a7dba9
Matthew Alverson <mda2635@gail.com>
2020-09-10 23:24:31 -0400

    Fix style issues in all subsystems

--- src/main/java/frc/robot/subsystems/ColorWheel.java
+++ src/main/java/frc/robot/subsystems/ColorWheel.java
@@ -20,40 +20,40 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ColorWheel extends SubsystemBase {
   private final I2C.Port i2cPort = I2C.Port.kOnboard;
-  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
-  private final ColorMatch m_colorMatcher = new ColorMatch();
+  private final ColorSensorV3 colorSensor = new ColorSensorV3(i2cPort);
+  private final ColorMatch colorMatcher = new ColorMatch();
 
-  private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
-  private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
-  private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
-  private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+  private final Color blueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+  private final Color greenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+  private final Color redTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+  private final Color yellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
 
-  private PWMTalonSRX m_spinner = new PWMTalonSRX(2);
+  private PWMTalonSRX spinner = new PWMTalonSRX(2);
 
   /**
    * Creates a new ColorWheel.
    */
   public ColorWheel() {
-    m_colorMatcher.addColorMatch(kBlueTarget);
-    m_colorMatcher.addColorMatch(kGreenTarget);
-    m_colorMatcher.addColorMatch(kRedTarget);
-    m_colorMatcher.addColorMatch(kYellowTarget);   
+    colorMatcher.addColorMatch(blueTarget);
+    colorMatcher.addColorMatch(greenTarget);
+    colorMatcher.addColorMatch(redTarget);
+    colorMatcher.addColorMatch(yellowTarget);   
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    Color detectedColor = m_colorSensor.getColor();
+    Color detectedColor = colorSensor.getColor();
 
     String colorString;
-    ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
-    if (match.color == kBlueTarget) {
+    ColorMatchResult match = colorMatcher.matchClosestColor(detectedColor);
+    if (match.color == blueTarget) {
       colorString = "Blue";
-    } else if (match.color == kRedTarget) {
+    } else if (match.color == redTarget) {
       colorString = "Red";
-    } else if (match.color == kGreenTarget) {
+    } else if (match.color == greenTarget) {
       colorString = "Green";
-    } else if (match.color == kYellowTarget) {
+    } else if (match.color == yellowTarget) {
       colorString = "Yellow";
     } else {
       colorString = "Unknown";
@@ -66,34 +66,39 @@ public class ColorWheel extends SubsystemBase {
   }
 
   public void spin() {
-    m_spinner.set(1.0);
+    spinner.set(1.0);
   }
 
   public void spinSlow() {
-    m_spinner.set(0.30);
+    spinner.set(0.30);
   }
 
   public void stop() {
-    m_spinner.set(0);
+    spinner.set(0);
   }
 
   public void reverse() {
-    m_spinner.set(-1.0);
+    spinner.set(-1.0);
   }
 
+  /**
+   * Determines the closest color to the defined colors.
+   * Converts color to a String representing the name of the color 
+   * @return String color
+   */
   public String getColor() {
-    Color detectedColor = m_colorSensor.getColor();
+    Color detectedColor = colorSensor.getColor();
 
     String colorString;
-    ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+    ColorMatchResult match = colorMatcher.matchClosestColor(detectedColor);
 
-    if (match.color == kBlueTarget) {
+    if (match.color == blueTarget) {
       colorString = "Blue";
-    } else if (match.color == kRedTarget) {
+    } else if (match.color == redTarget) {
       colorString = "Red";
-    } else if (match.color == kGreenTarget) {
+    } else if (match.color == greenTarget) {
       colorString = "Green";
-    } else if (match.color == kYellowTarget) {
+    } else if (match.color == yellowTarget) {
       colorString = "Yellow";
     } else {
       colorString = "Unknown";
@@ -101,10 +106,14 @@ public class ColorWheel extends SubsystemBase {
     return colorString;
   }
 
+  /**
+   * Returns the color assigned by the field.
+   * @return String representing the full name of the color.
+   */
   public String getAssignedColor() {
     String gameData;
     gameData = DriverStation.getInstance().getGameSpecificMessage();
-    if(gameData.length() > 0)
+    if (gameData.length() > 0)
     {
       switch (gameData.charAt(0))
       {
