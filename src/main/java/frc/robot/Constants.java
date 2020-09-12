15234aa3a4285b399f9380d3655b1d72c9144620
Matthew Alverson <mda2635@gail.com>
2020-09-11 21:07:18 -0400

    Pulling constants from master

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,7 +6,6 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot;
-import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -17,74 +16,40 @@ import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-    public static final class DriveConstants {
-        public static final int kLeftMotor1Port = 7;
-        public static final int kLeftMotor2Port = 6;
-        public static final int kRightMotor1Port = 10;
-        public static final int kRightMotor2Port = 4;
-    
-        // public static final int[] kLeftEncoderPorts = new int[]{0, 1};
-        // public static final int[] kRightEncoderPorts = new int[]{2, 3};
-        public static final boolean kLeftEncoderReversed = false;
-        public static final boolean kRightEncoderReversed = true;
-    
-        public static final double kTrackwidthMeters = 0.584;
-        public static final DifferentialDriveKinematics kDriveKinematics =
-            new DifferentialDriveKinematics(kTrackwidthMeters);
-    
-        public static final int kEncoderCPR = 42;
-        public static final double kWheelDiameterMeters = 0.152;
-        public static final double kEncoderDistancePerPulse =
-            // Assumes the encoders are directly mounted on the wheel shafts
-            (kWheelDiameterMeters * Math.PI) / (double) kEncoderCPR;
-    
-        public static final boolean kGyroReversed = true;
-    
-        // These are example values only - DO NOT USE THESE FOR YOUR OWN ROBOT!
-        // These characterization values MUST be determined either experimentally or theoretically
-        // for *your* robot's drive.
-        // The Robot Characterization Toolsuite provides a convenient tool for obtaining these
-        // values for your robot.
-        public static final double ksVolts = 0.103;
-        public static final double kvVoltSecondsPerMeter = 1.0;
-        public static final double kaVoltSecondsSquaredPerMeter = 0.139;
-    
-        // Example value only - as above, this must be tuned for your drive!
-        public static final double kPDriveVel = 8.5;
-      }
-    
-      public static final class OIConstants {
-        public static final int kDriverControllerPort = 1;
-      }
-    
-      public static final class AutoConstants {
-        public static final double kMaxSpeedMetersPerSecond = 3;
-        public static final double kMaxAccelerationMetersPerSecondSquared = 3;
-    
-        // Reasonable baseline values for a RAMSETE follower in units of meters and seconds
-        public static final double kRamseteB = 2;
-        public static final double kRamseteZeta = 0.7;
-      }
 
-       public static final class WenchConstants {
-        public static final int kMotorPort = 11;
-        public static final double kMaxSpeedUp = 0.5;
-        public static final double kMaxSpeedDown = -0.5;
-        public static final int kLowerLimitSwitchPort = 3;
-        public static final int kUpperLimitSwitchPort = 4;
-    }
+  public static final class DriveConstants {
+    public static final int kLeftMotor1Port = 7;
+    public static final int kLeftMotor2Port = 6;
+    public static final int kRightMotor1Port = 10;
+    public static final int kRightMotor2Port = 4;
 
-    public static final class ArmConstants {
-        public static final int kMotorPort = 1;
-        public static final int kLowerLimitSwitchPort = 0;
-        public static final int kUpperLimitSwitchPort = 1;
-        public static final int kSafetyLimitSwitchPort = 2;
-        public static final double kMaxSpeedUp = 0.40;
-        public static final double kMaxSpeedDown = -0.60;
-    }
+    public static final double kRampRate = 1.0; // Seconds to go from 0 to full speed
+    public static final double kMaxHighSpeed = 1.0;
+    public static final double kMaxLowSpeed = 0.25;
 
-    public static final class HopperConstants {
-        public static final int kMotorPort = 0;
-        public static final double kMaxSpeed = 1;
-    }
-   }
+    public static final int maxAmps = 50;
+    public static final int maxSecAmps = 80;
+  }
+
+  public static final class WenchConstants {
+    public static final int kMotorPort = 11;
+    public static final double kMaxSpeedUp = 0.5;
+    public static final double kMaxSpeedDown = -0.5;
+    public static final int kLowerLimitSwitchPort = 3;
+    public static final int kUpperLimitSwitchPort = 4;
+  }
+
+  public static final class ArmConstants {
+    public static final int kMotorPort = 1;
+    public static final int kLowerLimitSwitchPort = 0;
+    public static final int kUpperLimitSwitchPort = 1;
+    public static final int kSafetyLimitSwitchPort = 2;
+    public static final double kMaxSpeedUp = 0.40;
+    public static final double kMaxSpeedDown = -0.60;
+  }
+
+  public static final class HopperConstants {
+    public static final int kMotorPort = 0;
+    public static final double kMaxSpeed = 1;
+  }
+}
