1420e9bcaa18de1c79afbb2b409cf46fe4bda7a6
Matthew Alverson <mda2635@gmail.com>
2020-11-13 21:40:48 -0500

    Converting Shooter SubSytem to a PID subsystem.  Adding commands to the SmartDashboard

--- src/main/java/frc/robot/commands/Shoot.java
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -26,7 +26,8 @@ public class Shoot extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    shooter.spin(this.speed);
+    shooter.setSetpoint(this.speed);
+    shooter.enable();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -37,7 +38,7 @@ public class Shoot extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    shooter.stop();
+    shooter.disable();
   }
 
   // Returns true when the command should end.
